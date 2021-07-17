283bc48725f99dba1867b620b3a760dd2f23da59
linoy elimeleh <lino1998y@gmail.com>
2021-07-17 10:19:30 +0300

    test

---
+++ 2020_SemesterA_MoedB/src/test/Dictionary.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Set;
+
+public class Dictionary {
+
+	// you can add code here
+	
+	public Dictionary(String text) {
+		// you can add code here
+	}
+	
+	public Set<String> getSet(char c){
+		// implement in O(1)
+		return null;
+	}
+}
