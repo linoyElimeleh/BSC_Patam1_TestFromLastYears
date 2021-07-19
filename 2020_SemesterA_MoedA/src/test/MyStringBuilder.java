5ada474503386dc01e1f01e9d247130bee54d207
linoy elimeleh <lino1998y@gmail.com>
2021-07-19 08:37:37 +0300

    2020 semster a moed a

--- 2020_SemesterA_MoedA/src/test/MyStringBuilder.java
+++ 2020_SemesterA_MoedA/src/test/MyStringBuilder.java
@@ -5,28 +5,48 @@ import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class MyStringBuilder {
-	// ---------- DO NOT Change ----------------
-	ArrayList<Character> buff;
-	
-	public MyStringBuilder() {
-		buff=new ArrayList<>();
-	}
-	
-	public MyStringBuilder(char...ts) {
-		buff=new ArrayList<>();
-		for(char t : ts)
-			buff.add(t);
-	}
-	
-	// -----------------------------------------
-	
-	// implement append
-	
-	// implement reverse
-	
-	// implement apply
-	
-	// implement toString
+    // ---------- DO NOT Change ----------------
+    ArrayList<Character> buff;
 
-	
+    public MyStringBuilder() {
+        buff = new ArrayList<>();
+    }
+
+    public MyStringBuilder(char... ts) {
+        buff = new ArrayList<>();
+        for (char t : ts)
+            buff.add(t);
+    }
+
+    // -----------------------------------------
+
+    public MyStringBuilder append(String s) {
+        for (char c : s.toCharArray())
+            buff.add(c);
+        return this;
+    }
+
+    public MyStringBuilder reverse() {
+        ArrayList<Character> t = buff;
+        buff = new ArrayList<>();
+        for (int i = t.size() - 1; i >= 0; i--)
+            buff.add(t.get(i));
+        return this;
+    }
+
+
+    public String toString() {
+        char[] cs = new char[buff.size()];
+        for (int i = 0; i < cs.length; i++)
+            cs[i] = buff.get(i);
+        return new String(cs);
+    }
+
+    public MyStringBuilder apply(UnaryOperator<Character> uop) {
+        ArrayList<Character> t = buff;
+        buff = new ArrayList<>();
+        for (int i = 0; i < t.size(); i++)
+            buff.add(uop.apply(t.get(i)));
+        return this;
+    }
 }
--- 2020_SemesterA_MoedA/src/test/Q2.java
+++ 2020_SemesterA_MoedA/src/test/Q2.java
@@ -1,10 +1,42 @@
 package test;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Q2 {
 
-	public static void writeWords(String source, String destination) {
-		// impelemt
-		// don't forget to catch all exceptions
-	}
+    public static void writeWords(String source, String destination) {
+        try {
+            HashMap<String, Integer> map = new HashMap<>();
+            Scanner s = new Scanner(new BufferedReader(new FileReader(source)));
+            while (s.hasNext()) {
+                String w = s.next();
+                if (!map.containsKey(w))
+                    map.put(w, 1);
+                else
+                    map.put(w, map.get(w) + 1);
+            }
+            s.close();
+
+            List<String> l = new ArrayList<>();
+            l.addAll(map.keySet());
+
+            l.sort((s1, s2) -> {
+                int x = map.get(s2) - map.get(s1);
+                if (x == 0)
+                    x = s1.compareTo(s2);
+                return x;
+            });
+
+            PrintWriter out = new PrintWriter(new FileWriter(destination));
+            l.forEach(si -> out.println(si));
+            out.close();
+
+        } catch (IOException e) {
+        }
+    }
 }
